0dd003b315bf6343b943958e4e29c0fe50133f80
m0nch1 <m0nch1.develop@gmail.com>
2022-09-29 20:26:19 +0900

    fix yml

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,12 @@
 module.exports = {
   ci: {
-    "collect": {
-      "numberOfRuns": 3,
-      "url": [
+    collect: {
+      numberOfRuns: 3,
+      url: [
         "https://m0nch1-stores.com/"
       ]
     },
-    "upload": {
+    upload: {
       target: 'lhci',
       serverBaseUrl: 'https://railway-lhci-server-production.up.railway.app',
       token: '02ba037d-efd8-4f64-a3c7-e03cf17e14c7',
