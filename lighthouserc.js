8113ff2520cfe62cbcf4f56157301872836c175d
m0nch1 <m0nch1.develop@gmail.com>
2022-09-29 19:34:11 +0900

    fix: node version & env

--- lighthouserc.js
+++ lighthouserc.js
@@ -9,7 +9,7 @@ module.exports = {
     "upload": {
       target: 'lhci',
       serverBaseUrl: 'https://railway-lhci-server-production.up.railway.app',
-      token: process.env.LHCI_TOKEN,
+      token: '02ba037d-efd8-4f64-a3c7-e03cf17e14c7',
     },
   },
 };
